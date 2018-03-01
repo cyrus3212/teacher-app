f2988614a259feeba199fcd770607782671cd4ff
cyrus <cyrus@coderepubliq.com>
2018-03-01 18:57:33 +0800

    initial commit

---
+++ src/app/add-student-dialog/add-student-dialog.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import {MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material';
+
+@Component({
+  selector: 'app-add-student-dialog',
+  templateUrl: './add-student-dialog.component.html',
+  styleUrls: ['./add-student-dialog.component.scss']
+})
+export class AddStudentDialogComponent implements OnInit {
+
+  constructor(
+    public dialogRef: MatDialogRef<AddStudentDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { }
+
+  ngOnInit() {
+  }
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule, MatRadioModule} from '@angular/material';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { MatFormFieldModule, MatInputModule, MatSelectModule } from '@angular/material';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import 'hammerjs';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { AddStudentDialogComponent } from './add-student-dialog/add-student-dialog.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddStudentDialogComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule, 
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatCardModule,
+    MatTableModule,
+    FormsModule,
+    MatRadioModule,
+    ReactiveFormsModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatDialogModule,
+    MatToolbarModule,
+    MatSelectModule
   ],
   providers: [],
+  entryComponents:[AddStudentDialogComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
